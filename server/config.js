8c600c5338533d6661fa3cedef30c777be27a286
Hernan Pablo Keena <hernan@ei067.inti.gob.ar>
2015-05-15 15:40:06 -0300

    config

---
+++ server/config.js
@@ -0,0 +1,13 @@
+var config = {
+	google: {
+		APIKey: "??",
+		url: "https://www.googleapis.com/books/v1"
+	},
+	appSecret: "Secret!",
+
+	db: {
+		filename: "./db/users.db"
+	}
+}
+
+module.exports = config;
